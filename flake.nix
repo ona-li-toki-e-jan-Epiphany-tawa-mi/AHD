4581ef0c58d3e06d6b00e213e1a1c0dcf02398b6
jan Epiphany <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2025-03-06 20:57:05 -0500

    Applied nixfmt to flake.nix

--- flake.nix
+++ flake.nix
@@ -20,18 +20,14 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
-    let inherit (nixpkgs.lib) genAttrs systems;
+    let
+      inherit (nixpkgs.lib) genAttrs systems;
 
-        forAllSystems = f: genAttrs systems.flakeExposed (system: f {
-          pkgs = import nixpkgs { inherit system; };
-        });
+      forAllSystems = f:
+        genAttrs systems.flakeExposed
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
     in {
-      devShells = forAllSystems ({ pkgs }: {
-        default = with pkgs; mkShell {
-          packages = [
-            cbqn
-          ];
-        };
-      });
+      devShells = forAllSystems
+        ({ pkgs }: { default = with pkgs; mkShell { packages = [ cbqn ]; }; });
     };
 }
